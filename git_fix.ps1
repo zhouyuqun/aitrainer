f8fea07f6d0b4d5fc857786921c77ea11aa74bea
zhouyuqun <76272749@qq.com>
2026-05-09 16:09:33 +0800

    Create git_fix.ps1

---
+++ git_fix.ps1
@@ -0,0 +1,4 @@
+Set-Location "C:\Users\76272\Documents\GitHub\aitrainer"
+$git = "C:\Users\76272\AppData\Local\GitHubDesktop\app-3.5.8\resources\app\git\cmd\git.exe"
+& $git add "1.1.2/index.html"
+& $git status --short
